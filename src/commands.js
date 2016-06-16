5486e0bd2463044ad5ebfb921c6b08faaea016b0
Sam Lau <esayemm@gmail.com>
2016-06-16 00:49:48 -0700

    add usage and help

---
+++ src/commands.js
@@ -0,0 +1,18 @@
+import packageJson from '../package.json'
+
+export function usage() {
+  console.log(`${packageJson.name} ${packageJson.version}`)
+  console.log(``)
+  console.log(`Usage: ${Object.keys(packageJson.bin)[0]} [OPTIONS] <INPUT> <OUTPUT>`)
+  console.log(``)
+  console.log(`Move js, style, images and updates path references in project directory`)
+  console.log(``)
+  console.log(`Options:`)
+  console.log(``)
+  console.log(`  -n    do not update path references (just use mv)`)
+  console.log(``)
+}
+
+export function version() {
+  console.log(`${packageJson.name} ${packageJson.version}`)
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('hello world')
+const argv = require('yargs').argv
+import fs from 'fs'
+import * as commands from './commands.js'
+
+export function fileExists(file) {
+  try {
+    fs.accessSync(file, fs.F_OK)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+function main() {
+  const command = argv._[0]
+
+  // help/version stuff
+  if (argv.v || argv.version || ~['version', 'v'].indexOf(command))  {
+    commands.version()
+    return
+  } else if (argv.help || argv.h || ~['help', 'h'].indexOf(command)) {
+    commands.usage()
+    return
+  }
+
+  // mv logic
+  const input = argv._[0]
+  const output = argv._[1]
+
+  if (!input || !output) {
+    console.log(`Must provide input and output`)
+    return
+  }
+  if (!fileExists(input)) {
+    console.log(`${input} is not a valid file path`)
+    return
+  }
+}
+
+main()
