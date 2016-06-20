c0ad66da7eda60294744dd03566fc06875a1a49c
Sam Lau <esayemm@gmail.com>
2016-06-20 01:15:15 -0700

    change pathfinder api

--- src/pathfinder.js
+++ src/pathfinder.js
@@ -1,10 +1,36 @@
 import path from 'path'
 
-export function refactorPath({
-  inputPath,
-  inputFile,
-  changedFile,
+/**
+ * A file importing other files changed locations. Refactor import/require
+ * statements to be accurate with existing modules relative to changed file
+ * location.
+ */
+export function importerRefactor({
+  importStatement,
+  originalLocation,
+  changedLocation,
 }) {
+  const importModuleAbsolutePath = path.resolve(originalLocation, '..', importStatement)
+  const tempNodes = path.relative(changedLocation, importModuleAbsolutePath)
+  .split('/')
+  tempNodes.shift()
+  const importModuleRelativeFromChangedFile = tempNodes.join('/')
+  return importModuleRelativeFromChangedFile
+}
 
-  return ''
+/**
+ * A file that is imported by other files changed locations, Refactor
+ * import/require statement from existing importer file location relatived to
+ * changed module location.
+ */
+export function importRefactor({
+  importerLocation,
+  changedLocation,
+}) {
+  const importModuleAbsolutePath = path.resolve(changedLocation)
+  const tempNodes = path.relative(importerLocation, changedLocation)
+  .split('/')
+  tempNodes.shift()
+  const modifiedImportStatement = tempNodes.join('/')
+  return modifiedImportStatement
 }
