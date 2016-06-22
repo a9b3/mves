e791e270af765c083baafe4962897515c0ec55b5
Sam Lau <esayemm@gmail.com>
2016-06-21 19:28:54 -0700

    test cases for pathfinder

--- test/pathfinder.spec.js
+++ test/pathfinder.spec.js
@@ -1,33 +1,69 @@
 import should from 'should'
 import path from 'path'
 import * as pathfinder from '../src/pathfinder.js'
+import * as helper from './helper.js'
 
 describe('pathfinder', () => {
-
-  it('importerRefactor', async () => {
-    const importStatement = '../two/foo-module.js'
-    const originalLocation = path.resolve(__dirname, './foo/one/test.js')
-    const changedLocation = path.resolve(__dirname, './foo/three/four/test.js')
-
-    const refactoredPath = pathfinder.importerRefactor({
-      importStatement,
-      originalLocation,
-      changedLocation,
+  before(done => {
+    helper.setupDirectories()
+    .then(() => {
+      done()
     })
-    should.exist(refactoredPath)
-    refactoredPath.should.equal('../../two/foo-module.js')
+    .catch(done)
   })
 
-  it('importRefactor', async () => {
-    const importerLocation = path.resolve(__dirname, './foo/one/test.js')
-    const changedLocation = path.resolve(__dirname, './foo/two/test.js')
-
-    const refactoredImportStatement = pathfinder.importRefactor({
-      importerLocation,
-      changedLocation,
+  after(done => {
+    helper.removeDirectories()
+    .then(() => {
+      done()
     })
-    should.exist(refactoredImportStatement)
-    refactoredImportStatement.should.equal('../two/test.js')
+    .catch(done)
+  })
+
+  describe('getImportPath', () => {
+
+    it('css url', async () => {
+      const cssUrl = `background-image: url(./foo);`
+      const cssPath = pathfinder.getImportPath(cssUrl)
+      should.exist(cssPath)
+      cssPath.should.equal(`./foo`)
+    })
+
+    it('css import', async () => {
+      const cssImport = `
+      @import './foo'
+      `
+      const cssPath = pathfinder.getImportPath(cssImport)
+      should.exist(cssPath)
+      cssPath.should.equal(`./foo`)
+    })
+
+    it('require', async () => {
+      const requireStatement = `const foo = require('./foo');`
+      const requirePath = pathfinder.getImportPath(requireStatement)
+      should.exist(requirePath)
+      requirePath.should.equal('./foo')
+    })
+
+    it('import', async () => {
+      const importStatement = `import foo from './foo'`
+      const importPath = pathfinder.getImportPath(importStatement)
+      should.exist(importPath)
+      importPath.should.equal('./foo')
+    })
+
+    it('multiline import', async () => {
+      const importStatement = `
+      import {
+        foo,
+        bar,
+      } from './foo'
+      `.trim()
+      const importPath = pathfinder.getImportPath(importStatement)
+      should.exist(importPath)
+      importPath.should.equal('./foo')
+    })
+
   })
 
 })
