f1d3ecdd139d663fb061aee719bd913ada62775e
Sam Lau <esayemm@gmail.com>
2016-06-22 20:00:16 -0700

    add test cases for main functionality

--- test/main.spec.js
+++ test/main.spec.js
@@ -7,68 +7,68 @@ import {
 import { moveAndRefactor } from '../src/index.js'
 
 describe('main cli', () => {
-  const testDirName = 'testdir'
-  const testDir = path.resolve(__dirname, testDirName)
 
-  beforeEach(done => {
-    helper.setupDirectories(testDir)
-    .then(() => {
-      done()
+  describe('moveAndRefactor', () => {
+    const testDirName = 'testdir'
+    const testDir = path.resolve(__dirname, testDirName)
+    beforeEach(done => {
+      helper.setupDirectories(testDir)
+      .then(() => {
+        done()
+      })
+      .catch(done)
     })
-    .catch(done)
-  })
 
-  afterEach(done => {
-    helper.removeDirectories(testDir)
-    .then(() => {
-      done()
+    afterEach(done => {
+      helper.removeDirectories(testDir)
+      .then(() => {
+        done()
+      })
+      .catch(done)
     })
-    .catch(done)
-  })
 
-  it('moveAndRefactor', async () => {
-    const input = path.resolve(testDir, 'src')
-    const output = path.resolve(testDir, 'foo')
-    await moveAndRefactor({
-      input,
-      output,
+    it('moving importer file, imports should resolve', async () => {
+      const input = path.resolve(testDir, 'src')
+      const output = path.resolve(testDir, 'foo')
+      await moveAndRefactor({
+        input,
+        output,
+      })
+      fileExists(output).should.equal(true)
+      fileExists(path.resolve(output, 'index.js')).should.equal(true)
+      const testModule = require(path.resolve(output, 'index.js'))
+      should.exist(testModule.foo)
+      testModule.foo.name.should.equal('foo')
     })
-    fileExists(output).should.equal(true)
-    fileExists(path.resolve(output, 'index.js')).should.equal(true)
-    const testModule = require(path.resolve(output, 'index.js'))
-    should.exist(testModule.foo)
-    testModule.foo.name.should.equal('foo')
-  })
 
-  it('moveAndRefactor a module, importer should still be ok', async () => {
-    const input = path.resolve(testDir, 'modules', 'foo')
-    const output = path.resolve(testDir, 'bar')
-    await moveAndRefactor({
-      input,
-      output,
+    it('moving a module, files importing should still be ok', async () => {
+      const input = path.resolve(testDir, 'modules', 'foo')
+      const output = path.resolve(testDir, 'bar')
+      await moveAndRefactor({
+        input,
+        output,
+      })
+      fileExists(output).should.equal(true)
+      fileExists(path.resolve(output, 'index.js')).should.equal(true)
+      fileExists(path.resolve(testDir, 'src', 'index.js')).should.equal(true)
+
+      const testModule = require(path.resolve(testDir, 'src', 'index.js'))
+      testModule.foo.name.should.equal('foo')
     })
-    fileExists(output).should.equal(true)
-    fileExists(path.resolve(output, 'index.js')).should.equal(true)
-    fileExists(path.resolve(testDir, 'src', 'index.js')).should.equal(true)
 
-    const testModule = require(path.resolve(testDir, 'src', 'index.js'))
-    testModule.foo.name.should.equal('foo')
-  })
+    it('moving a directory of modules, importer should still work', async () => {
+      const input = path.resolve(testDir, 'modules')
+      const output = path.resolve(testDir, 'zed')
+      await moveAndRefactor({
+        input,
+        output,
+      })
+      fileExists(output).should.equal(true)
 
-  it('moveAndRefactor a directory of modules, importer should still work', async () => {
-    const input = path.resolve(testDir, 'modules')
-    const output = path.resolve(testDir, 'src')
-    await moveAndRefactor({
-      input,
-      output,
+      const testModule = require(path.resolve(testDir, 'src', 'index.js'))
+      testModule.foo.name.should.equal('foo')
+      testModule.one.name.should.equal('one')
+      testModule.two.name.should.equal('two')
     })
-    fileExists(output).should.equal(true)
-    fileExists(path.resolve(output, 'index.js')).should.equal(true)
-    fileExists(path.resolve(testDir, 'src', 'index.js')).should.equal(true)
-
-    const testModule = require(path.resolve(testDir, 'src', 'index.js'))
-    testModule.foo.name.should.equal('foo')
-    testModule.one.name.should.equal('one')
-    testModule.two.name.should.equal('two')
   })
 })
